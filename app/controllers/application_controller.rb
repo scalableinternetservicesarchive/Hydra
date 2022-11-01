e1b80fb6e4a5b990682d939894bc9e16eac8debd
YEthYuan <maxwell.yuanyeh@gmail.com>
2022-11-01 10:33:38 -0700

    Add find current user function to class ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
+  helper_method :current_user
+
+  def current_user
+    # Look up the current user based on user_id in the session cookie:
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
