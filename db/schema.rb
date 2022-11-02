9fe8b96542d967cf839a36f02954ae095da447f6
AndyCheng2 <luqingcheng2@gmail.com>
2022-11-01 18:22:34 -0700

    feat: generate comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_012425) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_012426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "Username"
+    t.string "Message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "Username"
