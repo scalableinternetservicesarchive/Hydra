4c4c35f36f412a0b8968fc2564659438ccc892e6
YEthYuan <maxwell.yuanyeh@gmail.com>
2022-11-03 15:26:17 -0700

    Ported session related files from Project3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,25 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+    session[:user_name] = user.name
+  end
+
+  # def authenticate(user)
+  #   if
+  # end
+
+  # def current_user
+  #   $current_user ||= User.find_by(id: session[:user_id])
+  #   puts " ++DEBUG++ #{$current_user}"
+  # end
+
+  def logged_in?
+    !@current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    session.delete(:user_name)
+    @current_user = nil
+  end
 end
