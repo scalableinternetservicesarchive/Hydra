db2a3dac8c2dca24c3ea76cbacc51c74b5e12bf1
YEthYuan <maxwell.yuanyeh@gmail.com>
2022-11-03 15:27:59 -0700

    modified user model to fit in login session settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :about, presence: true, length: { minimum: 10 }
 end
