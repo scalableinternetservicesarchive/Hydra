eb96a02c25f599d2c283e161365b4e0105a04d48
YEthYuan <maxwell.yuanyeh@gmail.com>
2022-11-03 17:11:00 -0700

    debug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:session][:name])
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(name: params[:session][:username])
+    if user && user.authenticate(params[:session][:password_digest])
       # puts " ++DEBUG++ into_login?"
       current_user
       log_in(user)
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
-    session[:user_name] = user.name
+    session[:user_name] = user.username
   end
 
   # def authenticate(user)
