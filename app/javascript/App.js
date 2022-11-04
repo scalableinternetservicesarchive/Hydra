bf497d48e617ddfd72df67ec5f8ead0598b1adf6
JerrySu11 <zihaosu@ucsb.edu>
2022-11-04 14:26:01 -0700

    set up React

---
+++ app/javascript/App.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import 'App.css';
+const HelloMessage = ({ name }) => <h1>Hello, {name}!</h1>;
+export default HelloMessage;
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import HelloMessage from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+document.addEventListener('DOMContentLoaded', () => {
+  root.render(<HelloMessage name="World" />);
+});
