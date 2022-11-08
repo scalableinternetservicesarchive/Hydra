27c7891a7957b68d359a98afbb1335cf2d44b4c0
YEthYuan <maxwell.yuanyeh@gmail.com>
2022-11-08 12:03:03 -0800

    debug

--- test/controllers/group_users_controller_test.rb
+++ test/controllers/group_users_controller_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class GroupUsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get group_users_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get group_users_create_url
-    assert_response :success
-  end
 end
