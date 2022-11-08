2baa974a613321992e4c0a51e0e4aa34b13dd109
YEthYuan <maxwell.yuanyeh@gmail.com>
2022-11-08 10:39:13 -0800

    dev show.

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -2,7 +2,17 @@ class GroupUsersController < ApplicationController
   include GroupUsersHelper
 
   def join
-    @group_user = GroupUser.new
+    group_user = GroupUser.new
+    group_user.user_id = current_user.id
+    group_user.group_id = params[:gid]
+    if group_user.save
+      flash[:notice] = "Join successfully!"
+    else
+      flash[:notice] = "Oops, couldn't join at this point. Please try again later."
+    end
+
+    redirect_to current_user
+
   end
 
 end
