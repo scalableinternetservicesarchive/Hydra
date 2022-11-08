c90143155d1c5e963b9fd5afaca6b022dff7e8eb
YEthYuan <maxwell.yuanyeh@gmail.com>
2022-11-08 11:56:12 -0800

    add group list to user's profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     # @posts = Post.where(name: @user.name)
+    @groupusers = GroupUser.where(user_id: @user.id)
   end
 
   def new
