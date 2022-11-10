5d95c70837344cdd7b6cf86345cb02b5e22dd90d
YEthYuan <maxwell.yuanyeh@gmail.com>
2022-11-09 16:06:32 -0800

    checkpoint

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,28 +2,24 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
     session[:user_name] = user.username
-    current_user.update(status: 'online')
+    user_params = User.find(session[:user_id]).as_json
+    user_params['status'] = 'online'
+    User.update(session[:user_id], user_params)
+    logger.info "++DEBUG++ inspect: #{User.find(session[:user_id]).inspect}"
     session[:status] = current_user.status
   end
-
-  # def authenticate(user)
-  #   if
-  # end
-
-  # def current_user
-  #   $current_user ||= User.find_by(id: session[:user_id])
-  #   puts " ++DEBUG++ #{$current_user}"
-  # end
 
   def logged_in?
     !current_user.nil?
   end
 
   def log_out
-    current_user.update(status: 'offline')
+    User.update(current_user.id, :status => 'offline')
+    logger.info "++DEBUG++ inspect: #{current_user.inspect}"
     session.delete(:user_id)
     session.delete(:user_name)
     session.delete(:status)
     @current_user = nil
   end
+
 end
