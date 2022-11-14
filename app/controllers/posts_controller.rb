81c2b9f2dd67b22aa10143a2b4c1785c532cba03
JerrySu11 <zihaosu@ucsb.edu>
2022-11-14 14:25:08 -0800

    bug fix for unauthenticated user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def index
     # display posts which are public (has group_id:nil), or are part of a group the user belongs to
-    group_user = GroupUser.where(user_id:current_user.id)
+    group_user = logged_in? ? GroupUser.where(user_id:current_user.id) : []
     arr = [nil]
     for g_u in group_user do
       arr.push(g_u.group_id)
